4b11f51234e1470e442cad6f10a7c38714ccc654
Alejandro <alejandrocursos98@gmail.com>
2020-01-16 20:17:48 +0100

    toString

--- JavaApplication2/src/javaapplication2/Jugador.java
+++ JavaApplication2/src/javaapplication2/Jugador.java
@@ -34,6 +34,11 @@ public class Jugador {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "numero=" + numero + ", nombre=" + nombre + '}';
+    }
     
     
     
