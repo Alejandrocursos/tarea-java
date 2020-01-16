c05171ecd7dadcea644963c75158f6660ab24b7e
Alejandro <alejandrocursos98@gmail.com>
2020-01-16 19:40:55 +0100

    Añade getters y setters

---
+++ JavaApplication2/src/javaapplication2/Jugador.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Jugador {
+
+    int numero;
+    String nombre;
+
+    public Jugador(int numero, String nombre) {
+        this.numero = numero;
+        this.nombre = nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
